5bc38192bfc69713af79f7d2ddc837cb2b9f25e8
Agustin Garate <agustin@spark6.com>
2024-08-05 15:10:17 -0300

    ADD balance

---
+++ src/components/home/balance.tsx
@@ -0,0 +1,96 @@
+import { Button } from "@rneui/themed";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { ImagesAssets } from "../../../assets/images/images";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { IonicIcons } from "../../data/types";
+
+function Balance() {
+  function RoundedButon({
+    onPress,
+    backgroundColor,
+    iconName,
+  }: {
+    onPress?: () => void;
+    backgroundColor: string;
+    iconName: IonicIcons;
+  }) {
+    return (
+      <Button
+        buttonStyle={{ backgroundColor: backgroundColor, marginHorizontal: 8 }}
+        icon={<Ionicons name={iconName} size={20}></Ionicons>}
+        radius={"xl"}
+        onPress={onPress}
+      />
+    );
+  }
+  return (
+    <View style={styles.card}>
+      <View style={styles.leftColumn}>
+        <View>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={styles.subtitle}>
+              <Text>Budget balance</Text>
+            </Text>
+            <Ionicons name="trending-up-outline" size={20} color={"#0acd00"} />
+          </View>
+
+          <Text style={styles.budgetText}>$1345.33</Text>
+        </View>
+
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <RoundedButon backgroundColor="#eeeeee" iconName="add" />
+          <RoundedButon backgroundColor="#ffe4d6" iconName="remove" />
+        </View>
+      </View>
+      <View style={styles.rightColumn}>
+        <Image style={styles.avatar} source={ImagesAssets.avatar} />
+
+        <Button
+          buttonStyle={styles.button}
+          titleProps={{}}
+          titleStyle={{ color: "#f9fffd" }}
+          radius="xl"
+          title="Details"
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 20,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rightColumn: {
+    alignItems: "center",
+  },
+  leftColumn: {
+    justifyContent: "space-between",
+  },
+  budgetText: {
+    fontSize: 30,
+    fontFamily: "Nunito-Bold",
+  },
+  subtitle: {
+    fontSize: 17,
+    color: "#8d8d8d",
+    fontFamily: "Nunito-Light",
+    paddingBottom: 5,
+    paddingRight: 5,
+  },
+  avatar: {
+    width: 70,
+    height: 70,
+    marginBottom: 25,
+  },
+  button: {
+    paddingHorizontal: 23,
+    backgroundColor: "#0cdc8c",
+  },
+});
+
+export default Balance;
--- src/screens/root/home/home_screen.tsx
+++ src/screens/root/home/home_screen.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import Balance from "../../../components/home/balance";
 
 function HomeScreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.screen}>
+      <Balance />
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    paddingTop: 50,
+  },
+});
 export default HomeScreen;
